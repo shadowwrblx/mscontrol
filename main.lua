8ce68bdeac685ebe7b99541342e33a73abc5f3fd
shadowwrblx <104780811+shadowwrblx@users.noreply.github.com>
2023-08-13 14:10:40 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,2 +1,4 @@
+-- modifying the code could result to a false blacklist, simply just dont.
+wait(math.random(1,10))
 script_key=getgenv().settings.key
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/225ae7b8d68f9d0bd7727e50949d2945.lua"))()

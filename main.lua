44de7a9cd33a1b43e998313ec9bda41de117aa72
shadowwrblx <104780811+shadowwrblx@users.noreply.github.com>
2023-08-13 14:21:07 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,4 +1,3 @@
 -- modifying the code could result to a false blacklist, simply just dont. delay is added to prevent false flags. remove it and you will be blacklisted without a rewhitelist.
 wait(math.random(1,10))
-script_key=getgenv().settings.key
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/225ae7b8d68f9d0bd7727e50949d2945.lua"))()

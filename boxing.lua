37d6ef28c2e68dff51c1c6821a6d802443b2ca98
shadowwrblx <104780811+shadowwrblx@users.noreply.github.com>
2023-09-17 12:40:01 +0200

    Delete boxing.lua

--- boxing.lua
+++
@@ -1,3 +0,0 @@
--- modifying the code could result to a false blacklist, simply just dont. its added to prevent false flags. remove it and you will be blacklisted without a rewhitelist.
-script_key=script_key
-if not getgenv().MSBoxing then repeat wait() until game:IsLoaded() loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/309f96e99e620d82e1cf654483c1135d.lua"))() getgenv().MSBoxing = true end
